6fa5c83a12646e2a38f6f143db6b2923de5f393a
Darosior <darosior@gmail.com>
2018-07-14 16:39:15 +0200

    Create exemple.py

---
+++ exemple.py
@@ -0,0 +1,6 @@
+# coding: utf8
+
+# Generate a basic (nothing compressed) Bitcoin "account" : an address and its corresponding private key
+kp = get_keypair()
+print("Address : ", kp[1])
+print("Private key : ", kp[0])
