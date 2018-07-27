73d4d5a517e63c58ea0d7957180a8e6c53a750fb
Darosior <darosior@gmail.com>
2018-07-27 17:56:10 +0200

    Compression had to be from the private key

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from keys import *
 
 # Returns a tuple of an address and its corresponding private key
 def get_keypair(compressed=True):
-	pk = gen_privkey()
-	addr = get_address(get_pubkey(pk, compressed))
-	return (wif_encode(pk.to_bytes(sizeof(pk), 'big'), compressed), addr)
+	pk = gen_privkey(compressed)
+	addr = get_address(get_pubkey(pk))
+	return (wif_encode(pk.to_bytes(sizeof(pk), 'big')), addr)
