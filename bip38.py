b660c600f27ba7ad69f12f53b0a00e452f542cea
Darosior <darosior@gmail.com>
2018-07-28 15:47:55 +0200

    Added decrypt() and improved testing

--- bip38.py
+++ bip38.py
@@ -22,7 +22,7 @@ def encrypt(key, passphrase):
         key = key.to_bytes(sizeof(key), 'big')
     # Then we set the flag depending of whether the key is compressed or not
     if key[len(key)-1] == 0x01: # Is compressed
-        key = key[:len(key)-1]
+        #key = key[:len(key)-1]
         flag = 0xe0.to_bytes(1, 'big')  # 11100000
     else: # Not compressed
         flag = 0xc0.to_bytes(1, 'big')  # 11000000
@@ -42,26 +42,118 @@ def encrypt(key, passphrase):
     encryptedhalf2 = cipher.encrypt( int2.to_bytes(sizeof(int2), 'big') )
     return base58check_encode(flag + addresshash + encryptedhalf1 + encryptedhalf2, 0x0142.to_bytes(2, 'big'))
 
+# Takes <key> as str and <passphrase> as str. Returns the decrypted private key (bytes).
+def decrypt(key, passphrase):
+    # Doing the reverse scheme
+    dec = base58check_decode(key, n=2) # Version is 2 bytes length, cf encrypt()
+    flag = dec[:1]
+    addresshash = dec[1:5]
+    encryptedhalf1 = dec[5:21]
+    encryptedhalf2 = dec[21:37]
+    passderived = scrypt.hash(passphrase.encode(), addresshash, 16384, 8, 8)
+    derivedhalf1 = passderived[0:32]
+    derivedhalf2 = passderived[32:64]
+    cipher = AES.new(derivedhalf2)
+    decryptedhalf1 = cipher.decrypt(encryptedhalf1)
+    decryptedhalf2 = cipher.decrypt(encryptedhalf2)
+    # Inverse of XOR is ... XOR
+    privkey = int.from_bytes(decryptedhalf1+decryptedhalf2, 'big') ^ int.from_bytes(derivedhalf1, 'big')
+    privkey = privkey.to_bytes(sizeof(privkey), 'big')
+    if flag == 0xe0.to_bytes(1, 'big'): # Compressed
+        privkey = privkey + 0x01.to_bytes(1, 'big')
+        address = get_address(get_pubkey(privkey))
+    elif flag == 0xc0.to_bytes(1, 'big'): # Not compressed
+        address = get_address(get_pubkey(privkey))
+    else:
+        raise ValueError("Key passed as first parameter was malformed. Couldn't read flag byte.")
+    if double_sha256(address.encode(), bin=True)[:4] == addresshash:
+        return privkey # type : bytes
+    else:
+        return False
+
 def test():
-    print("No compression : ")
-    print("hex : ", encrypt(0xCBF4B9F70470856BB4F40F80B87EDB90865997FFEE6DF315AB166D713AF433A5, 'TestingOneTwoThree'))
-    print("wif : ", encrypt('5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR', 'TestingOneTwoThree'))
-    print("expected : 6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg")
+    print("Test with no compression : ")
+    print(" Test 1")
+    hex = encrypt(0xCBF4B9F70470856BB4F40F80B87EDB90865997FFEE6DF315AB166D713AF433A5, 'TestingOneTwoThree')
+    wif = encrypt('5KN7MzqK5wt2TP1fQCYyHBtDrXdJuXbUzm4A9rKAteGu3Qi5CVR', 'TestingOneTwoThree')
+    expected = '6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg'
+    decrypted = decrypt(expected, 'TestingOneTwoThree')
+    if hex == expected and wif == expected:
+        print("     Encryption OK")
+    else:
+        print("     Encryption failed")
+        print("     hex : ", hex)
+        print("     wif : ", wif)
+        print("     expected : ", expected)
+    if decrypted and int.from_bytes(decrypted, 'big') == 0xCBF4B9F70470856BB4F40F80B87EDB90865997FFEE6DF315AB166D713AF433A5:
+        print("     Decryption OK")
+    else:
+        print("     Decryption Failed")
+        print("     decrypted : ", decrypted)
+
     print("")
-    print("hex : ", encrypt(0x09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE, 'Satoshi'))
-    print("wif : ", encrypt('5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5', 'Satoshi'))
-    print("expected : 6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq")
+
+    print(" Test 2")
+    hex = encrypt(0x09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE, 'Satoshi')
+    wif = encrypt('5HtasZ6ofTHP6HCwTqTkLDuLQisYPah7aUnSKfC7h4hMUVw2gi5', 'Satoshi')
+    expected = '6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq'
+    decrypted = decrypt(expected, 'Satoshi')
+    if hex == expected and wif == expected:
+        print("     Encryption OK")
+    else:
+        print("     Encryption failed")
+        print("     hex : ", hex)
+        print("     wif : ", wif)
+        print("     expected : ", expected)
+    if decrypted and int.from_bytes(decrypted, 'big') == 0x09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE:
+        print("     Decryption OK")
+    else:
+        print("     Decryption Failed")
+        print("     decrypted : ", decrypted)
+
     print("")
+
     print("")
-    print("With compression : ")
-    # Here I don't know why there is no 0x01 for compressed privkey
-    print("hex : ", encrypt(0xCBF4B9F70470856BB4F40F80B87EDB90865997FFEE6DF315AB166D713AF433A5, 'TestingOneTwoThree'))
-    print("wif : ", encrypt('L44B5gGEpqEDRS9vVPz7QT35jcBG2r3CZwSwQ4fCewXAhAhqGVpP', 'TestingOneTwoThree'))
-    print("expected : 6PYNKZ1EAgYgmQfmNVamxyXVWHzK5s6DGhwP4J5o44cvXdoY7sRzhtpUeo")
+    print("Test with compression : ")
+    print(" Test 1")
+    # Here I don't know why there is no 0x01 for compressed privkey so I added it
+    hex = encrypt(0xCBF4B9F70470856BB4F40F80B87EDB90865997FFEE6DF315AB166D713AF433A501, 'TestingOneTwoThree')
+    wif = encrypt('L44B5gGEpqEDRS9vVPz7QT35jcBG2r3CZwSwQ4fCewXAhAhqGVpP', 'TestingOneTwoThree')
+    expected = '6PYNKZ1EAgYgmQfmNVamxyXVWHzK5s6DGhwP4J5o44cvXdoY7sRzhtpUeo'
+    decrypted = decrypt(expected, 'TestingOneTwoThree')
+    if hex == expected and wif == expected:
+        print("     Encryption OK")
+    else:
+        print("     Encryption failed")
+        print("     hex : ", hex)
+        print("     wif : ", wif)
+        print("     expected : ", expected)
+    if decrypted and int.from_bytes(decrypted, 'big') == 0xCBF4B9F70470856BB4F40F80B87EDB90865997FFEE6DF315AB166D713AF433A501:
+        print("     Decryption OK")
+    else:
+        print("     Decryption Failed")
+        print("     decrypted : ", decrypted)
+
     print("")
-    print("hex : ", encrypt(0x09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE, 'Satoshi'))
-    print("wif : ", encrypt('KwYgW8gcxj1JWJXhPSu4Fqwzfhp5Yfi42mdYmMa4XqK7NJxXUSK7', 'Satoshi'))
-    print("expected : 6PYLtMnXvfG3oJde97zRyLYFZCYizPU5T3LwgdYJz1fRhh16bU7u6PPmY7")
+
+    print(" Test 2")
+    # Here I don't know why there is no 0x01 for compressed privkey so I added it
+    hex = encrypt(0x09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE01, 'Satoshi')
+    wif = encrypt('KwYgW8gcxj1JWJXhPSu4Fqwzfhp5Yfi42mdYmMa4XqK7NJxXUSK7', 'Satoshi')
+    expected = '6PYLtMnXvfG3oJde97zRyLYFZCYizPU5T3LwgdYJz1fRhh16bU7u6PPmY7'
+    decrypted = decrypt(expected, 'Satoshi')
+    if hex == expected and wif == expected:
+        print("     Encryption OK")
+    else:
+        print("     Encryption failed")
+        print("     hex : ", hex)
+        print("     wif : ", wif)
+        print("     expected : ", expected)
+    if decrypted and int.from_bytes(decrypted, 'big') == 0x09C2686880095B1A4C249EE3AC4EEA8A014F11E6F986D0B5025AC1F39AFBD9AE01:
+        print("     Decryption OK")
+    else:
+        print("     Decryption Failed")
+        print("     decrypted : ", decrypted)
 
 if __name__ == '__main__':
     test()
