18d2b445f92205d16d8258455b60e5840ddfe92b
Darosior <darosior@gmail.com>
2018-08-14 19:09:57 +0200

    Cleaning
    Added clearer docstrings and removed import *

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 # coding: utf8
-from keys import *
-from bip38 import *
+from utils import wif_encode, sizeof
+from keys import gen_privkey, get_pubkey, get_address
+from bip38 import encrypt, decrypt
 
-# Returns a tuple of an address and its corresponding private key
 def get_keypair(compressed=True):
+    """Generates a new Bitcoin keypair.
+    
+    Args:
+        compressed (bool): if True, the public key will be compressed.
+    
+    Returns:
+        tuple: the private key and the corresponding address, both as str.
+    """
     pk = gen_privkey(compressed)
     addr = get_address(get_pubkey(pk))
     return (wif_encode(pk.to_bytes(sizeof(pk), 'big')), addr)
 
 
-# Returns a private key encrypted with the passphrase as str. key can be str, int or bytes. passphrase must be str.
 def bip38_encrypt(key, passphrase):
+    """Encrypts a private key with a passphrase, as specified in bip38.
+    
+    Args:  
+        key (str|int|bytes): the private key to encrypt.
+        passphrase (str): the passphrase that will be needed to decrypt the key.
+        
+    Returns:
+        str: the encrypted private key.
+    """
     if not isinstance(key, int) and not isinstance(key, str) and not isinstance(key, bytes):
         raise ValueError("<key> must be int bytes or str")
     if isinstance(key, str): # If a hex-key is passed as str
@@ -24,10 +40,17 @@ def bip38_encrypt(key, passphrase):
     return encrypt(key, passphrase) # type : str
 
 
-# Returns a private key if succeeds to decrypt, False otherwise.
-# key can be str, int or bytes. passphrase must be str.
-# If bin is set to true, the raw key will be returned.
 def bip38_decrypt(key, passphrase, bin=False):
+    """Decrypts a bip38-encrypted Bitcoin private key.
+    
+    Args:
+        key (str|int|byte): key to decrypt.
+        passphrase (str): the passphrase used to encrypt the key.
+        bin (bool): if True, returns the Bitcoin private key decrypted as bytes.
+        
+    Returns:
+        int : the Bitcoin private key decrypted.
+    """
     if not isinstance(key, int) and not isinstance(key, str) and not isinstance(key, bytes):
         raise ValueError("<key> must be int bytes or str")
     if isinstance(key, int): # If a hex-key is passed as str
