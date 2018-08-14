b37516700d766e2291968afa396fae419b6254c5
Darosior <darosior@gmail.com>
2018-08-14 19:44:46 +0200

    Cleaning and a little fix in wif_decode
    Removed comments and added clearer docstrings
    Removed import *
    Best detection of compressed private key in wif_decode()

--- utils.py
+++ utils.py
@@ -1,22 +1,32 @@
 # coding: utf8
-from hashlib import *
+from hashlib import sha256, new
 from math import log
 from random import randint
 from time import time
 from math import log, floor
-from hashlib import *
 from requests import get
 
 
-# To get the size in bytes of an integer, https://stackoverflow.com/questions/14329794/get-size-of-integer-in-python
 def sizeof(n):
+	"""get the size in bytes of an integer, https://stackoverflow.com/questions/14329794/get-size-of-integer-in-python
+	
+	Args:
+            n (int): the integer to get the size
+	    
+	Returns:
+	    int: the size in bytes of the first parameter.
+	"""
 	if n == 0:
 		return 1
 	return int(log(n, 256)) + 1
 
 
-# Generates a pseudo-random int
 def gen_random():
+	"""Generates a pseudo-random integer.
+
+	Returns:
+	    int: a pseudo-random number.
+	"""
 	h = sha256()
 	# 2 or 3 differents sources of entropy
 	h.update(str(randint(0, pow(2, 256))).encode())
@@ -30,8 +40,16 @@ def gen_random():
 		return int(h.hexdigest(), 16)
 
 
-# Returns the ripemd160(sha256(bytes)), used a lot in Bitcoin. If bin is True, returns bytes, else returns str.
 def hash160(bytes, bin=False):
+    """Returns the ripemd160(sha256(data)), used a lot in Bitcoin.
+    
+    Args:
+        bytes (bytes): the data to hash.
+	bin (bool): if set to True, returns bytes.
+    
+    Returns:
+        str/bytes: the hash of the data passed as first parameter.
+    """
     rip = new('ripemd160')
     rip.update(sha256(bytes).digest())
     if bin:
@@ -40,8 +58,16 @@ def hash160(bytes, bin=False):
     	return rip.hexdigest() # type : str
 
 
-# Returns the sha256(sha256(bytes)), also used a lot. If bin is True, returns bytes, else returns str.
 def double_sha256(bytes, bin=False):
+    """Returns the sha256(sha256(data)), used a lot in Bitcoin.
+    
+    Args:
+        bytes (bytes): the data to hash.
+	bin (bool): if set to True, returns raw data
+
+    Returns:
+        str/bytes: the hash of the data passed as first parameter.
+    """
     h = sha256(bytes)
     if bin:
         return sha256(h.digest()).digest() # type : bytes
@@ -49,8 +75,15 @@ def double_sha256(bytes, bin=False):
         return sha256(h.digest()).hexdigest() # type : str
 
 
-# Takes a number (hex or dec) and returns its base58_encoding
 def base58_encode(n):
+    """Takes a number (hex or dec) and returns its base58_encoding.
+    
+    Args:
+    	n (int): the number to base58_encode.
+	
+    Returns:
+    	str: the number passed as first parameter, base58 encoded.
+    """
     alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
     x = n % 58
     rest = n // 58
@@ -60,8 +93,15 @@ def base58_encode(n):
         return base58_encode(rest) + alphabet[x]
 
 
-# Takes a string (base58 encoded number) and returns an int (the base10 number)
 def base58_decode(string):
+    """Takes a base58-encoded number and returns it in base10.
+    
+    Args:
+    	string (str): the number to base58_decode.
+	
+    Returns:
+    	int: the number passed as first parameter, base10 encoded.
+    """
     alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
     # Populating a dictionary with base58 symbol chart
     dict = {}
@@ -79,8 +119,16 @@ def base58_decode(string):
     return n
 
 
-# Returns the base58check_encoded data, with prefix "version". <n> and <version> bytes
 def base58check_encode(n, version):
+	"""Returns the base58check_encoded data, with prefix "version".
+	
+	Args:
+	    n (bytes): the data to base58check encode.
+	    version (bytes): the prefix to set during encoding.
+	
+	Returns:
+	    str: the data encoded in base58check.
+	"""
 	shasha = double_sha256(version+n) #str
 	checksum = int(shasha[:8], 16).to_bytes(4, 'big') # First four bytes
 	if int.from_bytes(version, 'big') == 0: # Else leading zeros are wiped
@@ -89,9 +137,17 @@ def base58check_encode(n, version):
 		return base58_encode(int.from_bytes(version+n+checksum, 'big'))
 
 
-# Takes a string and returns bytes. <n> (int) specify the number of prefix bytes
-# The "zero" parameter is to specify if the string specified was encoded with a 0x00 prefix
-def base58check_decode(string, n=1, zero=False):
+def base58check_decode(string, n=1, zero=False):*
+    """Returns the base58check_decoded data. Please notice the "zero" parameter.
+	
+    Args:
+        string (str): the data to decode.
+	n (int): the prefix's size (in bytes). 1 in most cases.
+        zero (bool): to set to True if the string specified was encoded with a 0x00 prefix.
+	
+    Returns:
+        str: the data encoded in base58check.
+    """
     data = base58_decode(string)
     if zero:
         return data.to_bytes(sizeof(data), 'big')[:sizeof(data)-4]
@@ -99,14 +155,29 @@ def base58check_decode(string, n=1, zero=False):
         return data.to_bytes(sizeof(data), 'big')[n:sizeof(data) - 4]
 
 
-# WIF-encode data (as bytes) provided. If the data (which likely be a private key) corresponds to a compressed pk
 def wif_encode(data):
+	"""WIF-encode data (which would likely be a Bitcoin private key) provided.
+	
+	Args:
+	    data (bytes): the data to encode.
+	    
+	Returns:
+	    str: the WIF-encoded data.
+	"""
 	return base58check_encode(data, 0x80.to_bytes(1, 'big'))
 
 
-# WIF-decode string and returns bytes
-def wif_decode(string, compressed=False):
+def wif_decode(string):
+    """WIF-decode string (which would likely be a WIF-encoded Bitcoin private key) provided.
+	
+    Args:
+        string (str): the data to decode.
+	    
+    Returns:
+        str: the WIF-encoded data.
+    """
     dec = base58check_decode(string)
+    compressed = string[0] == 'K' or string[0] == 'L'
     if compressed:
         return dec[:len(dec)-1]
     else:
